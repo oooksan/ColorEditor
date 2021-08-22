080ab0dfc0db0e84c29141866e0189404776133c
oooksan <89106064+oooksan@users.noreply.github.com>
2021-08-22 18:29:00 +0300

    logic for sliders

--- ColorEditor/ViewController.swift
+++ ColorEditor/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
 
+    @IBAction func redSliderAction() {
+        redLabel.text = String(round(100 * redSlider.value)/100)
+        editColorViewBackground()
+    }
 
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(round(100 * greenSlider.value)/100)
+        editColorViewBackground()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(round(100 * blueSlider.value)/100)
+        editColorViewBackground()
+    }
+    
+    private func editColorViewBackground() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
 }
 
